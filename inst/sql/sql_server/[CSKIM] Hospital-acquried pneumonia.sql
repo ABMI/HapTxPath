1458ceb95d3d680e4abdb8a8e621d07a425eae23
Chungsoo Kim <ted9219@ajou.ac.kr>
2020-08-09 14:36:20 +0000

    add missing ABx

--- inst/sql/sql_server/[CSKIM] Hospital-acquried pneumonia.sql
+++ inst/sql/sql_server/[CSKIM] Hospital-acquried pneumonia.sql
@@ -72,11 +72,11 @@ UNION  select c.concept_id
 ) I
 LEFT JOIN
 (
-  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (42920760,19075378,19102471,40001157,40088730,35770401,40088728,40027153,43179753,1594403,40030894)
+  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (19102471,40001157,40088730,35770401,40088728,40027153,43179753,1594403,40030894,40028720)
 UNION  select c.concept_id
   from @vocabulary_database_schema.CONCEPT c
   join @vocabulary_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
-  and ca.ancestor_concept_id in (42920760,19075378,19102471,40001157,40088730,35770401,40088728,40027153,43179753,1594403,40030894)
+  and ca.ancestor_concept_id in (19102471,40001157,40088730,35770401,40088728,40027153,43179753,1594403,40030894,40028720)
   and c.invalid_reason is null
 
 ) E ON I.concept_id = E.concept_id

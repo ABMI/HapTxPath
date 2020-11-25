73f0cdeb9bd86342452462a2919909d301eca5e3
Chungsoo Kim <ted9219@ajou.ac.kr>
2020-11-25 14:20:53 +0000

    add cultureResult table

---
+++ R/cultureResult.R
@@ -0,0 +1,45 @@
+#' Run drugPathway package
+#'
+#' @details
+#' Run the drugPathway package, which implements the cohort pathway design.
+#'
+#' @param cdmDatabaseSchema    Schema name where your patient-level data in OMOP CDM format resides.
+#'                             Note that for SQL Server, this should include both the database and
+#'                             schema name, for example 'cdm_data.dbo'.
+#' @param cohortDatabaseSchema Schema name where intermediate data can be stored. You will need to have
+#'                             write priviliges in this schema. Note that for SQL Server, this should
+#'                             include both the database and schema name, for example 'cdm_data.dbo'.
+#' @param minCellCount        
+#'
+#' @import dplyr
+#' @export
+#' 
+
+#cultureResult
+
+#Profiling the germ etiology in cohort using fact_relationship table in CDM 5.3.1
+
+createCultureResultTable <- function(cdmDatabaseSchema,
+                          cohortDatabaseSchema,
+                          minCellCount = 5){
+  
+  pathToCsv <- system.file("settings", "CohortsToCreate.csv", package = "PneumoniaTxPath")
+  cohortsToCreate <- read.csv(pathToCsv)
+  cohortIds <- cohortsToCreate$cohortId
+  
+  path <- system.file(package = "PneumoniaTxPath", "sql", "sql_server", "BacteriaCulture.sql")
+  sql <- SqlRender::readSql(path)
+  sql <- SqlRender::render(sql, 
+                           cdm_database_schema = cdmDatabaseSchema,
+                           cohort_database_schema = cohortDatabaseSchema,
+                           vocabulary_database_schema = cdmDatabaseSchema,
+                           cohort_table = cohortTable,
+                           cohortId = cohortIds,
+                           minCount = minCellCount)
+  
+  cultureResult <- DatabaseConnector::querySql(connection = DatabaseConnector::connect(connectionDetails), sql)
+  cultureResult <- reshape2::dcast(data = cultureResult, COHORT_DEFINITION_ID + CONCEPT_NAME ~ MEASUREMENT_CONCEPT_ID, value.var = c("PERSONCOUNTS"))
+  cultureResult[is.na(cultureResult)] <- 0
+  
+  return(cultureResult)
+}
